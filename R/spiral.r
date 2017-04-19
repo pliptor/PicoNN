6c1ee73260a41ca60ccabed0d3da700d6652e1b1
pliptor <pliptor@users.noreply.github.com>
2017-04-19 14:21:15 -0700

    spiral.r R script produces new extras/input.png

--- R/spiral.r
+++ R/spiral.r
@@ -2,17 +2,32 @@
 # The train data is the spiral pattern used in the
 # cn231 course by Andrej Karpathy
 
-# The C++ code that generates it is located in
-# src/piconn.cpp 
+# The C++ code that generates it is located at 
+# https://github.com/pliptor/PicoNN 
 
-train <- read.csv("../extras/train.csv");
-test  <- read.csv("../extras/test.csv");
 
-train$color[train$Label==2] <- "red"
-train$color[train$Label==1] <- "blue"
-train$color[train$Label==0] <- "black"
+load_data <- function() {
+	path <- "../extras/";        #github repo
+	if(!dir.exists(path)) {
+		path <- "../input/"; #kaggle
+	}
+	assign("train", read.csv(paste0(path, "train.csv")), envir = .GlobalEnv);
+	assign("test",  read.csv(paste0(path, "test.csv")),  envir = .GlobalEnv);
+};
 
-plot(train$X, train$Y, col=train$color, main="Spiral Train", xlab="X", ylab="Y")
+load_data();
+
+train$color[train$Label==2] <- "darkred";
+train$color[train$Label==1] <- "blue";
+train$color[train$Label==0] <- "coral";
+
+plot(train$X, train$Y, bg=train$color, pch=21,  main="Spiral Train", xlab="X", ylab="Y");
+
+# write file 
+if(dir.exists("../extras/")) {
+	dev.copy(png,"../extras/input.png");
+	dev.off()
+}
 
 
 
