2d43b10d99eaa86bf2ef86954ae74338b12f65a3
pliptor <pliptor@users.noreply.github.com>
2017-04-19 19:13:45 -0700

    spiral.r R script produces new extras/svmoutput.png for svm classification with radial basis kernel.

--- R/spiral.r
+++ R/spiral.r
@@ -1,13 +1,18 @@
 # This script reads the train data and plots it.
+# Next, it makes an SVM model with radial basis kernel and plots the decision boundaries.
+#
 # The train data is the spiral pattern used in the
 # cn231 course by Andrej Karpathy
 
-# The C++ code that generates it is located at 
+# The C++ code that generates the train and test patterns is located at 
 # https://github.com/pliptor/PicoNN 
 
+input_image  <- "input.png";
+output_image <- "svmoutput.png";
+path <- "../extras/";
 
 load_data <- function() {
-	path <- "../extras/";        #github repo
+	path <- "../extras/";
 	if(!dir.exists(path)) {
 		path <- "../input/"; #kaggle
 	}
@@ -21,13 +26,36 @@ train$color[train$Label==2] <- "darkred";
 train$color[train$Label==1] <- "blue";
 train$color[train$Label==0] <- "coral";
 
+# plot train data
 plot(train$X, train$Y, bg=train$color, pch=21,  main="Spiral Train", xlab="X", ylab="Y");
 
-# write file 
-if(dir.exists("../extras/")) {
-	dev.copy(png,"../extras/input.png");
+# write input image file 
+if(dir.exists(path) && !file.exists(paste0(path, input_image))) {
+	dev.copy(png, paste0(path, input_image));
 	dev.off()
 }
 
+# transform Label column to factor so svm treats it as a categorical classification
+train$Label <- factor(train$Label, levels=c("0","1","2"), ordered=T);
 
+# load svm library 
+library(e1071);
+model <- svm(Label ~ ., data = train[,!colnames(train) %in% "color"]);
+print(model);
+summary(model);
 
+# predict and adjust offset so Labels return to the original set of {0, 1, 2}
+pred <- as.numeric(predict(model, test)) - 1;
+
+z <- matrix(pred, nrow = 100, byrow=F)
+# Make a contour plot for the decision boundaries. Also called "level plot"
+filled.contour(x = seq(-1, 1, length.out = nrow(z)),
+               y = seq(-1, 1, length.out = ncol(z)),
+	       z, levels = seq(-0.5, 3 , 1), col = c("coral","blue","darkred","black"),
+	       xlab="X", ylab="Y", main="SVM classification (kernel=radial basis)");
+
+# write input image file 
+if(dir.exists(path) && !file.exists(paste0(path, output_image))) {
+	dev.copy(png, paste0(path, output_image));
+	dev.off()
+}
