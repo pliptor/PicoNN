932f27ca89bfda865d69a7c530038e56606bbfcd
pliptor <pliptor@users.noreply.github.com>
2017-04-22 00:08:12 -0700

    Update R script to produce SVM solutions with different kernels. - linear kernel - sigmoid kernel - The sigmoid kernel result has symmetry issues.

--- R/spiral.r
+++ R/spiral.r
@@ -9,7 +9,6 @@
 # https://github.com/pliptor/PicoNN 
 
 input_image  <- "input.png";
-output_image <- "svmoutput.png";
 path <- "../extras/";
 
 load_data <- function(file) {
@@ -24,13 +23,13 @@ train <- load_data("train.csv");
 test  <- load_data("test.csv");
 
 # color list for each of the classes. The example has three classes but one extra is needed as required by filled.contour
-clist      <- rainbow(4, s = 1,   v = 1, start = 0.2, 1, alpha = 1)
+	clist      <- rainbow(4, s = 1,   v = 1, start = 0.2, 1, alpha = 1)
 clistdesat <- rainbow(4, s = 0.5, v = 1, start = 0.2, 1, alpha = 1)
 
-for (c in 1:3) {
-	train$color[train$Label==c-1] <- clist[c];
-	train$colordesat[train$Label==c-1] <- clistdesat[c];
-}
+	for (c in 1:3) {
+		train$color[train$Label==c-1] <- clist[c];
+		train$colordesat[train$Label==c-1] <- clistdesat[c];
+	}
 
 # plot train data
 plot(train$X, train$Y, bg=train$color , pch=21,  main="Spiral Train", xlab="X", ylab="Y");
@@ -46,23 +45,48 @@ train$Label <- factor(train$Label, levels=c("0", "1", "2"), ordered=T);
 
 # load svm library 
 library(e1071);
-model <- svm(Label ~ X + Y, data = train);
-print(model);
-summary(model);
+svm_model <- function(title="SVM", kernel='radial basis') {
+	model <- svm(Label ~ X + Y, data = train, kernel=kernel, coef0=0);
+	print(model);
+	summary(model);
+	plot_title = paste(title,'kernel',kernel);
 
 # predict and adjust offset so Labels return to the original set of {0, 1, 2}
-pred <- as.numeric(predict(model, test)) - 1;
+	pred <- as.numeric(predict(model, test)) - 1;
 
 # reshape prediction vector to a matrix to produce decision boundaries and areas
-z <- matrix(pred, nrow = sqrt(nrow(test)), byrow=F)
+	z <- matrix(pred, nrow = sqrt(nrow(test)), byrow=F)
 # Make a contour plot for the decision boundaries. Also called "level plot"
-filled.contour(x = seq(-1, 1, length.out = nrow(z)),
-               y = seq(-1, 1, length.out = ncol(z)),
-	       z, levels = seq(-0.5, 3 , 1), col = clistdesat,
-	       xlab="X", ylab="Y", main="SVM classification (kernel=radial basis)", plot.axes = {points(train$X, train$Y, bg=train$color, pch=21); axis(1); axis(2) });
+		filled.contour(x = seq(-1, 1, length.out = nrow(z)),
+				y = seq(-1, 1, length.out = ncol(z)),
+				z, levels = seq(-0.5, 3 , 1), col = clistdesat,
+				xlab="X", ylab="Y", main=plot_title, plot.axes = {points(train$X, train$Y, bg=train$color, pch=21); axis(1); axis(2) });
+}
 
+# default svm with radial basis kernel
+svm_model(kernel='radial')
+output_image <- "svmoutput.png";
 # write output image file 
 if(dir.exists(path) && !file.exists(paste0(path, output_image))) {
 	dev.copy(png, paste0(path, output_image));
 	dev.off()
 }
+
+# a linear kernel cannot handle the curved decision regions
+svm_model(kernel='linear')
+output_image <- "linearoutput.png";
+# write output image file 
+if(dir.exists(path) && !file.exists(paste0(path, output_image))) {
+	dev.copy(png, paste0(path, output_image));
+	dev.off()
+}
+
+# a linear kernel cannot handle the curved decision regions
+svm_model(kernel='sigmoid')
+output_image <- "sigmoidoutput.png";
+# write output image file 
+if(dir.exists(path) && !file.exists(paste0(path, output_image))) {
+	dev.copy(png, paste0(path, output_image));
+	dev.off()
+}
+
